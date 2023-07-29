ca469882e4c131f096a9fc7ceda47ce0badb0756
Alwyn Roy Miranda <75134499+alwynroy009@users.noreply.github.com>
2023-07-29 23:23:01 +0530

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,1 @@
-
-def sum_of_N_numbers(N):
-    return N*(N+1)//2
-
-sum_N = sum_of_N_numbers
-
-for _ in range(int(input().strip())):
-    n = int(input().strip())
-    n -= 1
-    print(3*sum_N(n//3) + 5*sum_N(n//5) - 15*sum_N(n//(15)))
-    
+# Work in Progress
