1de4188a731cec1073b701a0760723ea62ed7836
Alwyn Roy Miranda <75134499+alwynroy009@users.noreply.github.com>
2023-07-29 23:25:13 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,8 +3,7 @@ def sum_of_N_numbers(N):
 
 sum_N = sum_of_N_numbers
 
-for _ in range(int(input().strip())):
+for _ in range(int(input())):
     n = int(input().strip())
     n -= 1
     print(3*sum_N(n//3) + 5*sum_N(n//5) - 15*sum_N(n//15))
-    
